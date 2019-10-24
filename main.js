103bc5c23b2860e34a30245887ad27f11fee145b
ryokke <ryotahbb@icloud.com>
2019-10-25 00:17:55 +0900

    improve main.js

--- main.js
+++ main.js
@@ -8,9 +8,23 @@
 //   - weight: 体重
 // - インスタンスごとの固有の情報はコンストラクタ経由でセットする
 
+class Person {
+  constructor(name, age, gender, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.height = height;
+    this.weight = weight;
+  }
+}
 
 // 課題2: 自分もしくは、架空の人の情報を使って１人の人間を表すPersonインスタンスを生成する
 // - インスタンスは変数 `me` に代入する
-
+const me = new Person("りょっけ", 27, "male", 218, 60);
 
 // 課題3: 課題2で生成したインスタンスの情報をconsole.logを使って、「名前」「年齢」「性別」「身長」「体重」が正しくセットされていることを確認する
+console.log('me.nameはりょっけであるべき：', me.name);
+console.log('me.ageは27であるべき：', me.age);
+console.log('me.genderはmaleであるべき：', me.gender);
+console.log('me.heightは218であるべき：', me.height);
+console.log('me.weightは60であるべき：', me.weight);
